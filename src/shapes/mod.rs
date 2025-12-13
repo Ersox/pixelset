55768da08562ba19ba374d5d5d42ce089b117e48
Ersox <ersox.maps@gmail.com>
2025-12-13 11:14:35 -0500

    Add doc comment for `Shape` trait

--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -6,6 +6,10 @@ pub use crate::shapes::rectangle::Rectangle;
 mod rectangle;
 mod rectangle_outline;
 
+/// A geometric shape that can be represented as a set of pixels.
+///
+/// A `Shape` defines a region in 2D pixel space and provides multiple ways
+/// to interact with it, and convert it to a `PixelSet`.
 pub trait Shape {
     /// Generates a `PixelSet` containing all pixels inside the shape.
     fn set(&self) -> PixelSet;
